f06dfb0e8dab9a3a6db2b03279e43ae450abd77e
nitingarg20 <142803514+nitingarg20@users.noreply.github.com>
2026-07-18 07:52:43 +0000

    dataTypes

---
+++ basics_01/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 +3) // we are using nodejs, not browser
+
+console.log(3
+    +
+      3) // will run but not a good way to write a code
+
+console.log("Nitin")
+
+
+let name = "Nitin"
+let age = 21
+let isloggedIn = false
+
+// number => 2 to power of 53
+// bigInt for very large numbers
+// string ""
+// boolean = true or false
+// null => standalone value -> if does not find the value sends null
+// undefined => you do not assign the value like we did previously as let state; ->not value was assigned just variable was declared
+// symbol -> for uniqueness
+
+// object
+
+console.log(typeof age); // -> tells the type of the particular thing
+console.log(typeof null); // type is object
+console.log(typeof undefined); // undefined
+
+
