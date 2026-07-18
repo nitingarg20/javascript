32ffb179249503b14a86597ea800710f43d4eab8
nitingarg20 <142803514+nitingarg20@users.noreply.github.com>
2026-07-18 10:14:37 +0000

    conversions

--- basics_01/03_conversionOperation.js
+++ basics_01/03_conversionOperation.js
@@ -39,4 +39,29 @@ let stringNumber = String(someNumber);
 console.log(typeof stringNumber);
 console.log(stringNumber);
 
+// operations // 
 
+let value = 3
+let negValue = -value
+
+console.log(negValue);
+
+console.log(2**3); // makes it 2 to the power 3 
+// we can have all other operations as well like + - / * % 
+
+let str1 = "hello"
+let str2 = " Nitin"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 1); // 121
+console.log(2 + 1 + "2"); // 32
+
+let num1, num2, nums3
+num1 = nums2 = nums3 = 20 + 2
+
+let counter = 3
+++counter
+console.log(counter);
