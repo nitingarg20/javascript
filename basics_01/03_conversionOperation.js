5f7a0ebf7b7d108a8e2ca11140c2fd9f9cc6b9e8
nitingarg20 <142803514+nitingarg20@users.noreply.github.com>
2026-07-18 09:29:48 +0000

    Conversion of dataTypes

---
+++ basics_01/03_conversionOperation.js
@@ -0,0 +1,42 @@
+let score = false
+
+console.log(typeof(score))
+console.log(typeof score)
+
+let ValueInNumber = Number(score)
+
+console.log(typeof ValueInNumber);
+console.log(ValueInNumber);
+
+
+// when converting into a number
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// null => 0
+// undefined => NaN
+
+
+let IsLoggedIn = "";
+
+console.log(typeof IsLoggedIn);
+
+let BooleanLoggedIn = Boolean(IsLoggedIn);
+
+console.log(typeof BooleanLoggedIn);
+console.log(BooleanLoggedIn);
+
+// when converting into a boolean
+
+// 1 => true; 0 => false
+// "" => false
+// "Nitin" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
+
